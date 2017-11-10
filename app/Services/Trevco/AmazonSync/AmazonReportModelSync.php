c4d46f50b91da0bd2e31bfb5dfdc7697c7afe4d7
Nnorukah, Chukwuka C <nnorukah.c@gmail.com>
2017-11-10 18:46:41 +0100

    Stream order creation from file

--- app/Services/Trevco/AmazonSync/AmazonReportModelSync.php
+++ app/Services/Trevco/AmazonSync/AmazonReportModelSync.php
@@ -5,6 +5,9 @@ use App\Repositories\OrderRepository;
 use App\Services\Trevco\AmazonReportTransformer;
 use Exception;
 use ReflectionClass;
+use Prewk\XmlStringStreamer;
+use Prewk\XmlStringStreamer\Stream;
+use Prewk\XmlStringStreamer\Parser;
 
 class AmazonReportModelSync
 {
@@ -37,6 +40,31 @@ class AmazonReportModelSync
 	        	$createdOrder->orderItem()->createMany($orderItems);
 	        }
 		}			
+		return true;
+	}
+
+	public function saveModelsFromFile($modelName, $reportDataFile)
+	{
+		$model = \App::make($modelName);
+		$dataType = (new ReflectionClass($model))->getShortName();
+		$stream = new Stream\File($reportDataFile, 1024);
+		$parser = new Parser\StringWalker(['uniqueNode' => $dataType]);
+		$streamer =  XmlStringStreamer::createUniqueNodeParser($reportDataFile, array("uniqueNode" => $dataType));
+		while ($orderNode = $streamer->getNode()) {
+		    $order = camel_keys(json_decode(json_encode(simplexml_load_string($orderNode)), true));
+	        $orderItems = array_get($order, 'orderItem');
+	        dump($order['amazonOrderID']);
+	        $createdOrder = \App::make(OrderRepository::class)->updateOrCreate(array_only($order, ['amazonOrderID', 'merchantOrderID']),array_except($order, ['orderItem']));
+
+        	$createdOrder->orderItem()->delete();
+	        if (empty($orderItems[0])) {
+	        	$createdOrder->orderItem()->create($orderItems);
+	        }
+	        else {
+	        	$createdOrder->orderItem()->createMany($orderItems);
+	        }
+
+		}		
 		return true;
 	}
 }
