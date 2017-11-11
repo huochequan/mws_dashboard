5ab3190d8035e932090e6784a5a3086799506d0f
Nnorukah, Chukwuka C <nnorukah.c@gmail.com>
2017-11-11 09:05:05 +0100

    Refactor api call and Dashboard

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\AppBaseController;
 use App\Http\Requests\API\CreateOrderAPIRequest;
 use App\Http\Requests\API\UpdateOrderAPIRequest;
 use App\Order;
 use App\Repositories\OrderRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\Http\Controllers\AppBaseController;
 use InfyOm\Generator\Criteria\LimitOffsetCriteria;
 use Prettus\Repository\Criteria\RequestCriteria;
 use Response;
@@ -31,21 +32,50 @@ class OrderController extends AppBaseController
      */
     public function index(Request $request)
     {
-        // ini_set('max_excution_time', 0);
-        // ini_set('memory_limit','32M');
-        $this->orderRepository->pushCriteria(new RequestCriteria($request));
-        $this->orderRepository->pushCriteria(new LimitOffsetCriteria($request));
-        $orders = $this->orderRepository->all();
+        // $this->orderRepository->pushCriteria(new RequestCriteria($request));
+        // $this->orderRepository->pushCriteria(new LimitOffsetCriteria($request));
+        // $orders = $this->orderRepository->all();
+        // $ordersPayload = [];
+        // Order::chunk(50, function ($orders) use (&$ordersPayload)
+        // {
+        //     $ordersIDTotalMap = $orders->mapWithKeys(function ($order) {
+        //         return [$order->id => $order->total];
+        //     })->all();
+        //     $ordersArray = $orders->toArray();
+        //     foreach ($ordersArray as $key => $value) {
+        //         $value['total'] = $ordersIDTotalMap[$value['id']];
+        //         $ordersArray[$key] = $value;
+        //     }
+        //     $ordersPayload = array_merge($ordersPayload, $ordersArray);
+        // });
 
-        $ordersIDTotalMap = $orders->mapWithKeys(function ($order) {
-            return [$order->id => $order->total];
-        })->all();
-        $ordersPayload = $orders->toArray();
-        foreach ($ordersPayload as $key => $order) {
-            $order['total'] = $ordersIDTotalMap[$order['id']];
-            $ordersPayload[$key] = $order;
-        }
-        return $this->sendResponse($ordersPayload, 'Orders retrieved successfully');
+        $saleDaysRange = date_range(Carbon::now()->subDays(30)->startOfDay(), Carbon::now()->endOfDay());
+        $salesLast30Days = 0;
+        $salesToday = 0;
+        $salesYesterday = 0;
+        $today = Carbon::now()->toDateString();
+        $saleDaysData = array_map(function ($day) use (&$salesLast30Days, &$salesToday, &$salesYesterday)
+        {
+            $dayFBASales = 0;
+            $dayFBMSales = 0;
+            foreach (Order::where('purchaseDate', $day->toDateString())->cursor() as $order) {
+                $dayFBASales += ($order->fulfillmentData['fulfillmentChannel'] == "Amazon") ? $order->total :  0;
+                $dayFBMSales += ($order->fulfillmentData['fulfillmentChannel'] == "Merchant") ? $order->total: 0;
+            }
+            $salesLast30Days += $dayFBMSales + $dayFBASales;
+
+            if (Carbon::now()->toDateString() == $day->toDateString()) {
+                $salesToday += $dayFBMSales + $dayFBASales;
+            }
+            $salesToday += (Carbon::now()->toDateString() == $day->toDateString())? $dayFBMSales + $dayFBASales : 0;
+            $salesYesterday += (Carbon::yesterday()->toDateString() == $day->toDateString())? $dayFBMSales + $dayFBASales : 0;
+            return ['purchaseDate' => $day->toDateString(), 'dayFBASales' => $dayFBASales, 'dayFBMSales' => $dayFBMSales];
+        },$saleDaysRange);
+        $ordersToday = Order::where('purchaseDate', Carbon::today()->toDateString())->count();
+        $unshippedCount = Order::where('orderStatus','!=',"Shipped")->count();
+        // return $this->sendResponse($ordersPayload, 'Orders retrieved successfully');
+        return $this->sendResponse(compact('saleDaysData', 'salesLast30Days', 'salesToday', 'salesYesterday', 'ordersToday', 'unshippedCount'), 'Orders retrieved successfully');
+
     }
 
     /**
--- resources/assets/js/trevco_dash/views/Dashboard.vue
+++ resources/assets/js/trevco_dash/views/Dashboard.vue
@@ -6,7 +6,7 @@
       <div class="col-sm-12 col-lg-2">
         <b-card class="order-detail-card bg-darken" :no-block="true">
           <div class="card-body font-weight-bold pb-0">
-            <h1 class="order-data-value text-center mb-0">${{ salesToday }}</h1>
+            <h1 class="order-data-value text-center mb-0">${{ dashboardFormatted(salesToday) }}</h1>
             <p class="h5 order-data-caption text-center font-weight-bold">Sales today</p>
           </div>
         </b-card>
@@ -30,7 +30,7 @@
       <div class="col-sm-12 col-lg-2">
         <b-card class="order-detail-card bg-darken" :no-block="true">
           <div class="card-body font-weight-bold pb-0">
-            <h1 class="order-data-value text-center mb-0">${{ salesLast30Days }}</h1>
+            <h1 class="order-data-value text-center mb-0">${{ dashboardFormatted(salesLast30Days) }}</h1>
             <p class="h5 order-data-caption text-center font-weight-bold">Sales last 30 days</p>
           </div>
         </b-card>
@@ -46,7 +46,7 @@
       <div class="col-sm-12 col-lg-2">
         <b-card class="order-detail-card bg-darken" :no-block="true">
           <div class="card-body pb-0">
-            <h1 class="order-data-value text-center mb-0">{{ percentageDiffSalesYesterdaySign }} {{ percentageDiffSalesYesterday }}%</h1>
+            <h1 class="order-data-value text-center mb-0">{{ percentageDiffSalesYesterdaySign }} {{ percentageDiffSalesYesterday }}</h1>
             <p class="h5 order-data-caption text-center font-weight-bold">Yesterday</p>
           </div>
         </b-card>
@@ -58,13 +58,13 @@
     height="30px"
     />
     <b-card header="Sales($)" class="borderless order-card">
-        <orders-chart :width="1100" :orders="orders"/>
+        <!-- <orders-chart :width="1100" :orders="orders"/> -->
     </b-card>
   </div>
 </template>
 
 <script>
-import OrdersChart from '../components/OrdersChart';
+// import OrdersChart from '../components/OrdersChart';
 import axios from 'axios';
 import * as moment from 'moment';
 var arraySum = (x, y) =>{
@@ -92,19 +92,19 @@ var round2Fixed = (value) => {
   return kFormatter((+(value[0] + 'e' + (value[1] ? (+value[1] - 2) : -2))).toFixed(2));
 }
 
-var last30Days = (order) => { return moment(order.purchaseDate).isBetween(moment().startOf('day').subtract(30, 'day'), moment().endOf('day'))};
-var sameDay = (order) => { return moment().isSame(order.purchaseDate, 'day')};
-var yesterday = (order) => { return moment().subtract(1,'day').isSame(order.purchaseDate, 'day')};
-var unshippedByMerchant = (order) => { return (order.orderStatus !== "Shipped") && (order.fulfillmentData.fulfillmentChannel == "Merchant")};
-
 export default {
   name: 'dashboard',
   components: {
-    OrdersChart
+    // OrdersChart
   },
   data: function () {
     return {
-      orders: []
+      salesToday: 0,
+      salesYesterday: 0,
+      salesLast30Days: 0,
+      ordersToday: 0,
+      unshippedCount: 0,
+      saleDaysData: []
     }
   },
   methods: {
@@ -120,48 +120,37 @@ export default {
         $variant = 'danger'
       }
       return $variant
+    },
+
+    dashboardFormatted (value) {
+      return round2Fixed(value);
     }
   },
   mounted () {
     axios
       .get('/order')
       .then((response) => {
-        // console.log(response.data);
-        this.orders = response.data.data;
+        this.salesToday = response.data.data.salesToday,
+        this.salesYesterday = response.data.data.salesYesterday,
+        this.salesLast30Days = response.data.data.salesLast30Days,
+        this.ordersToday = response.data.data.ordersToday,
+        this.unshippedCount = response.data.data.unshippedCount,
+        this.saleDaysData = response.data.data.saleDaysData
       });
   },
   computed: {
-    salesTotal() {
-      var sT = this.orders.map(order => order.total);
-      return round2Fixed(sT.reduce(arraySum, 0));
-    },
-    salesToday() {
-      var sT = this.orders.filter(sameDay).map( order => order.total );
-      return round2Fixed(sT.reduce(arraySum, 0));
-    },
-    salesYesterday() {
-      var sT = this.orders.filter(yesterday).map(order => order.total);
-      return round2Fixed(sT.reduce(arraySum, 0));
-    },
-    salesLast30Days() {
-      var sT = this.orders.filter(last30Days).map(order => order.total);
-      return round2Fixed(sT.reduce(arraySum, 0));
-    },
-    orderCountLast30Days() {
-      return kFormatter(this.orders.filter(last30Days).length) || "--";
-    },
-    ordersToday() {
-      return kFormatter(this.orders.filter(sameDay).length) || "--";
-    },
-    percentageDiffSalesYesterday() {
-      return parseInt(Math.abs((this.orders.filter(sameDay).map( order => order.total ).reduce(arraySum, 0) - this.orders.filter(yesterday).map( order => order.total ).reduce(arraySum, 0)) / this.orders.filter(yesterday).map( order => order.total ).reduce(arraySum, 0) * 100)) || "--";
+     percentageDiffSalesYesterday() {
+      if (this.salesToday == 0) {
+        return "--";
+      }
+      return parseInt(Math.abs(this.salesToday - this.salesYesterday) / this.salesYesterday) + "%" || "--";
     },
     percentageDiffSalesYesterdaySign() {
-      return parseFloat(this.orders.filter(sameDay).map( order => order.total ).reduce(arraySum, 0)) > parseFloat(this.orders.filter(yesterday).map( order => order.total ).reduce(arraySum, 0)) ? "+" : parseFloat(this.orders.filter(sameDay).map( order => order.total ).reduce(arraySum, 0)) == parseFloat(this.orders.filter(yesterday).map( order => order.total ).reduce(arraySum, 0)) ? "" : "-";
+      if (this.salesToday == 0) {
+        return "";
+      }
+      return parseFloat(this.salesYesterday) > parseFloat(this.salesYesterday) ? "+" : parseFloat(this.salesToday) == parseFloat(this.salesYesterday) ? "" : "-";
     },
-    unshippedCount() {
-      return this.orders.filter(unshippedByMerchant).length || "--";
-    }
-  }
+ }
 }
 </script>
